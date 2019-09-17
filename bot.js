7de5b70f5046ad21958280bf660fd9e8df3070c1
Sven Fehler <sven.fehler@web.de>
2019-09-17 13:02:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,16 +5,16 @@ require("dotenv").config();
 
 
 client.on("error", function(err) {
-    // this is needed cause the Discord API is shitty and dies sometimes, causing the wrapper to throw an error and thus crashing the bot - with this function that won't happen
-    console.log("\n\n\x1b[31m\x1b[1m[Client Error]: \x1b[0m" + err.message);
+    // this is needed because the websocket connection to the Discord API dies sometimes, causing the wrapper to throw an error and thus crashing the bot - with this function that won't happen
+    console.log("\n\n\x1b[31m\x1b[1m[Client Error]: \x1b[0m" + err.message); // (the \x1b stuff is color codes)
 });
 
 
 
 client.on("ready", function() {
-    // This function gets executed once the bot is properly logged in and ready to interact and execute shit
+    // This function gets executed once the bot is properly logged in and ready to interact and execute stuff
 
-    client.user.setActivity("with your mom's tiddies", {type: "PLAYING"}); // set the bot's activity message here - type can be "PLAYING", "WATCHING", "LISTENING" and "STREAMING"
+    client.user.setActivity("type !ping", {type: "PLAYING"}); // set the bot's activity message here - type can be "PLAYING", "WATCHING", "LISTENING" and "STREAMING"
     client.user.setStatus("online"); // can be "idle", "online", "dnd" (do not disturb) or "invisible"
     client.user.setAvatar("https://example.org/example.png") // sets the profile picture
 
@@ -23,7 +23,7 @@ client.on("ready", function() {
     client.on("message", function(message) {
         // This function gets executed every time someone sends a message in a channel of a guild your bot is in
 
-        if(message.author.bot) return; // this is used to prevent other bots from executing commands. This could create infinite recursion in the worst case
+        if(message.author.bot) return; // this is used to prevent other bots from executing commands. This could create infinite loops in the worst case
         if(message.channel.type == "dm" || !message.guild) return; // these two checks are used to prevent users from executing commands inside the DMs
 
         if(message.content == "!ping") {
